b88ddce704ad3c68046ce7545676946deb3c4c2b
Scott A. Idler <scott.a.idler@gmail.com>
2021-12-20 19:26:38 -0800

    sadie

--- imap-filter.py
+++ imap-filter.py
@@ -19,9 +19,9 @@ REL = os.path.relpath(DIR, CWD)
 
 NAME, EXT = os.path.splitext(REAL_NAME)
 
-IMAP_DOMAIN = os.environ.get('IMAP_DOMAIN', 'imap.gmail.com')
-IMAP_USERNAME = os.environ.get('IMAP_USERNAME', 'scott.idler@tatari.tv')
-IMAP_PASSWORD = os.environ.get('IMAP_PASSWORD', None)
+IMAP_DOMAIN = os.environ.get('IMAP_DOMAIN')
+IMAP_USERNAME = os.environ.get('IMAP_USERNAME')
+IMAP_PASSWORD = os.environ.get('IMAP_PASSWORD')
 
 import logging
 
@@ -31,15 +31,20 @@ logging.basicConfig(
 )
 import email
 import json
+from addict import Addict
 from fnmatch import fnmatch
 from ruamel import yaml
 from datetime import datetime
 from collections import OrderedDict
 from backports.zoneinfo import ZoneInfo
+from leatherman.dbg import dbg
 from leatherman.yaml import yaml_print
-from leatherman.fuzzy import fuzzy
+from leatherman.dictionary import head_body
+from leatherman.repr import __repr__
 from imapclient import IMAPClient, RECENT
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+from leatherman import fuzzy
 
 def clean(s):
     if s != None:
@@ -51,18 +56,43 @@ def compare(test, items):
         return test == items
     return any([fnmatch(item, test) for item in items])
 
-class MessageFilter:
-    def __init__(self, to, cc, fr, move, star):
-        self.to = to
-        self.cc = cc
-        self.fr = fr
-        self.move = move
-        self.star = star
+class ScalarToMultipleError(Exception):
+    def __init__(self, obj):
+        msg = f'scalar to multiple error on obj={obj}'
+        super().__init__(msg)
 
-    def filter(self, message):
-        if self.cc and compare(self.cc, message.cc):
-            return False
-        #if compare(self.cc, message.cc) and
+def listify(obj):
+    if isinstance(obj, str) or isinstance(obj, int):
+        return [obj]
+    if obj == {} or obj == None:
+        return []
+    if isinstance(obj, tuple):
+        return list(obj)
+    if isinstance(obj, list):
+        return obj
+    raise ScalarToMultipleError(obj)
+
+def ensure_defaults(obj, matches=1):
+    return Addict(
+        patterns=obj.patterns if obj.patterns != {} else [],
+        matches=obj.matches if obj.matches != {} else matches
+    )
+
+def update(d, **kwargs):
+    d.update(**kwargs)
+    return d
+
+class MessageFilter:
+    def __init__(self, spec):
+        name, body = head_body(spec)
+        self.name = name
+        self.to = body.to if body.to != {} else None
+        self.cc = body.cc if body.cc != {} else None
+        self.fr = body.dr if body.fr != {} else None
+        self.move = body.move
+        self.star = body.star
+
+    __repr__ = __repr__
 
 class Message:
     def __init__(self, count, uid, data):
@@ -124,8 +154,51 @@ class Message:
                 'uid': self.uid,
                 'count': self.count,
             }.items()
-            if v
+            #if v
         })
+
+class IMAPFilter:
+    def __init__(self, imap_domain=None, imap_username=None, imap_password=None, filters=None, folders=None, **kwargs):
+        self.filters = filters
+        self.folders = folders
+        self.client = IMAPClient(imap_domain)
+        self.client.login(imap_username, imap_password)
+        self.client.select_folder('INBOX')
+
+    __repr__ = __repr__
+
+    @property
+    def messages(self):
+        gmail_search = 'to:scott.idler@tatari.tv and after:2021/12/12'
+        ids = self.client.gmail_search(gmail_search)
+        return [
+            Message(count, uid, data)
+            for count, (uid, data)
+            in enumerate(self.client.fetch(ids, 'RFC822').items())
+        ]
+
+    @property
+    def message_filters(self):
+        return [
+            MessageFilter(f)
+            for f
+            in self.filters
+        ]
+
+    def compare_message(self, message, message_filter):
+        if message_filter.to
+
+    def apply_filter(self, messages, message_filter):
+        for message in messages:
+
+        return messages
+
+    def execute(self):
+        messages = self.messages
+        for mf in self.message_filters:
+            messages = self.apply_filter(mf, messages)
+        self.client.shutdown()
+        print('execution complete')
 
 def main(args):
     parser = ArgumentParser(
@@ -139,41 +212,62 @@ def main(args):
         help='default="%(default)s"; config filepath')
     ns, rem = parser.parse_known_args(args)
     try:
-        config = {
+        config = Addict({
             key.replace('-', '_'): value
             for key, value
             in yaml.safe_load(open(ns.config)).items()
-        }
+        })
     except FileNotFoundError as er:
         config = dict()
     parser = ArgumentParser(
         parents=[parser])
     parser.set_defaults(**config)
     parser.add_argument(
-        '--domain',
-        default=IMAP_DOMAIN,
+        '--imap-domain',
+        default=os.environ.get('IMAP_DOMAIN', config.get('imap_domain')),
         help='default="%(default)s"; imap domain')
     parser.add_argument(
-        '--username',
-        default=IMAP_USERNAME,
+        '--imap-username',
+        default=os.environ.get('IMAP_USERNAME', config.get('imap_username')),
         help='default="%(default)s"; imap username')
     parser.add_argument(
-        '--password',
-        default=IMAP_PASSWORD,
+        '--imap-password',
+        default=os.environ.get('IMAP_PASSWORD', config.get('imap_password')),
         help='default="%(default)s"; imap password')
     ns = parser.parse_args()
-    print(ns)
 
-    with IMAPClient(ns.domain) as server:
-        server.login(ns.username, ns.password)
-        server.select_folder('INBOX', readonly=True)
-        messages = server.gmail_search('in:unread and after:2021/11/29')
-        messages = server.gmail_search('to:scott.idler@tatari.tv and after:2021/11/29')
-        for count, (uid, data) in enumerate(server.fetch(messages, 'RFC822').items()):
-            msg = Message(count, uid, data)
-            #yaml_print(dict(message=msg.json()))
-            yaml_print(msg.json())
-            print()
+    ifilter = IMAPFilter(**ns.__dict__)
+    ifilter.execute()
+#    assert ns.imap_password != None, 'imap-password is required'
+#    with IMAPClient(ns.imap_domain) as server:
+#        server.login(ns.imap_username, ns.imap_password)
+#        server.select_folder('INBOX', readonly=True)
+#        ids = server.gmail_search('to:scott.idler@tatari.tv and after:2021/12/12')
+#        messages = [
+#            Message(count, uid, data)
+#            for count, (uid, data)
+#            in enumerate(server.fetch(ids, 'RFC822').items())
+#        ]
+#        print('total:', len(messages))
+#        yaml_print([m.to for m in messages])
+#        print()
+#        for mf in [MessageFilter(f) for f in ns.filters]:
+#            if mf.to:
+#                messages1 = [
+#                    message
+#                    for message
+#                    in messages
+#                    if message.to == mf.to
+#                ]
+#            if mf.cc:
+#                messages1 = [
+#                    message
+#                    for message
+#                    in messages
+#                    if message.cc == mf.cc
+#                ]
+#            for m in messages1:
+#                yaml_print(m.json())
 
 if __name__ == '__main__':
     main(sys.argv[1:])
