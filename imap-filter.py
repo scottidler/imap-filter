edee8d60c38fdfebbae69cc79b938bf8f0720430
Scott A. Idler <scott.a.idler@gmail.com>
2021-12-28 14:16:10 -0800

    stable implementation

--- imap-filter.py
+++ imap-filter.py
@@ -44,7 +44,7 @@ from leatherman.repr import __repr__
 from imapclient import IMAPClient, RECENT
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-from leatherman import fuzzy
+from leatherman.fuzzy import fuzzy
 
 def clean(s):
     if s != None:
@@ -82,17 +82,91 @@ def update(d, **kwargs):
     d.update(**kwargs)
     return d
 
+class AddressFilterError(Exception):
+    def __init__(self, obj):
+        msg = f'address filter error obj={obj}'
+        super().__init__(msg)
+
+class AddressFilter:
+    def __init__(self, spec):
+        #self.spec = spec
+        if spec == None:
+            self.matches = None
+            self.patterns = None
+        elif isinstance(spec, dict):
+            self.matches = spec.get('matches', None)
+            self.patterns = spec.get('patterns', None)
+        elif isinstance(spec, list):
+            if any(map(lambda item: '*' in item, spec)):
+                self.matches = None
+                self.patterns = spec
+            else:
+                self.matches = spec
+                self.patterns = None
+        elif isinstance(spec, str):
+            if '*' in spec:
+                self.matches = None
+                self.patterns = [spec]
+            else:
+                self.matches = [spec]
+                self.patterns = None
+        else:
+            raise AddressFilterError(spec)
+
+    @property
+    def passthru(self):
+        return self.matches == None and self.patterns == None
+
+    __repr__ = __repr__
+
+    def compare(self, addresses):
+        if self.matches == None and self.patterns == None:
+            return True
+        if self.matches != None:
+            if sorted(self.matches) == sorted(addresses):
+                return True
+            if all(map(lambda m: m in addresses, self.matches)):
+                addresses = sub(addresses, self.matches)
+            else:
+                return False
+        if self.patterns != None:
+            matches = [
+                address
+                for address
+                in addresses
+                if any(map(lambda p: fnmatch(address, p), self.patterns))
+            ]
+            if matches:
+                addresses = sub(addresses, matches)
+                return True
+            return False
+        elif addresses:
+            return False
+        return True
+
 class MessageFilter:
     def __init__(self, spec):
         name, body = head_body(spec)
         self.name = name
-        self.to = body.to if body.to != {} else None
-        self.cc = body.cc if body.cc != {} else None
-        self.fr = body.dr if body.fr != {} else None
+        self.to = AddressFilter(body.to)
+        self.cc = AddressFilter(body.cc)
+        self.fr = AddressFilter(body.fr)
         self.move = body.move
         self.star = body.star
 
     __repr__ = __repr__
+
+    def compare(self, message):
+        if not self.to.compare(message.to):
+            return False
+        if not self.cc.compare(message.cc):
+            return False
+        if not self.fr.compare(message.fr):
+            return False
+        return True
+
+    def apply(self, message):
+        return self.compare(message)
 
 class Message:
     def __init__(self, count, uid, data):
@@ -122,7 +196,7 @@ class Message:
 
     @property
     def fr(self):
-        return self._emails(self._fr)[0]
+        return self._emails(self._fr)
 
     @property
     def sub(self):
@@ -154,18 +228,21 @@ class Message:
                 'uid': self.uid,
                 'count': self.count,
             }.items()
-            #if v
         })
+
+def sub(list1, list2):
+    return list(set(list1) - set(list2))
 
 class IMAPFilter:
     def __init__(self, imap_domain=None, imap_username=None, imap_password=None, filters=None, folders=None, **kwargs):
         self.filters = [
-            Message(f)
+            MessageFilter(f)
             for f
             in filters
         ]
         self.folders = folders
         self.client = IMAPClient(imap_domain)
+        assert imap_password != None, 'imap-password is required'
         self.client.login(imap_username, imap_password)
         self.client.select_folder('INBOX')
 
@@ -181,23 +258,38 @@ class IMAPFilter:
             in enumerate(self.client.fetch(ids, 'RFC822').items())
         ]
 
-    def compare_message(self, message, message_filter):
-        if message_filter.to and message_filter.to != {}:
-            pass
-
-    def apply_filter(self, messages, message_filter):
-        for message in messages:
-            if self.compare_message(message, message_filter):
-                pass
-
-        return messages
+    def apply_filter(self, message_filter, messages):
+        return [
+            message
+            for message
+            in messages
+            if message_filter.apply(message)
+        ]
 
     def execute(self):
         messages = self.messages
         for mf in self.filters:
-            messages = self.apply_filter(mf, messages)
+            filtered = self.apply_filter(mf, messages)
+            print(mf)
+            [
+                print(' ', message)
+                for message
+                in filtered
+            ]
+            print('*'*80)
+            messages = list(set(messages) - set(filtered))
         self.client.shutdown()
         print('execution complete')
+
+def load(config):
+    try:
+        return Addict({
+            key.replace('-', '_'): value
+            for key, value
+            in yaml.safe_load(open(config)).items()
+        })
+    except FileNotFoundError as er:
+        return dict()
 
 def main(args):
     parser = ArgumentParser(
@@ -210,14 +302,7 @@ def main(args):
         default='%(REL)s/%(NAME)s.yml' % globals(),
         help='default="%(default)s"; config filepath')
     ns, rem = parser.parse_known_args(args)
-    try:
-        config = Addict({
-            key.replace('-', '_'): value
-            for key, value
-            in yaml.safe_load(open(ns.config)).items()
-        })
-    except FileNotFoundError as er:
-        config = dict()
+    config = load(ns.config)
     parser = ArgumentParser(
         parents=[parser])
     parser.set_defaults(**config)
@@ -237,36 +322,6 @@ def main(args):
 
     ifilter = IMAPFilter(**ns.__dict__)
     ifilter.execute()
-#    assert ns.imap_password != None, 'imap-password is required'
-#    with IMAPClient(ns.imap_domain) as server:
-#        server.login(ns.imap_username, ns.imap_password)
-#        server.select_folder('INBOX', readonly=True)
-#        ids = server.gmail_search('to:scott.idler@tatari.tv and after:2021/12/12')
-#        messages = [
-#            Message(count, uid, data)
-#            for count, (uid, data)
-#            in enumerate(server.fetch(ids, 'RFC822').items())
-#        ]
-#        print('total:', len(messages))
-#        yaml_print([m.to for m in messages])
-#        print()
-#        for mf in [MessageFilter(f) for f in ns.filters]:
-#            if mf.to:
-#                messages1 = [
-#                    message
-#                    for message
-#                    in messages
-#                    if message.to == mf.to
-#                ]
-#            if mf.cc:
-#                messages1 = [
-#                    message
-#                    for message
-#                    in messages
-#                    if message.cc == mf.cc
-#                ]
-#            for m in messages1:
-#                yaml_print(m.json())
 
 if __name__ == '__main__':
     main(sys.argv[1:])
